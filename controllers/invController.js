ea2bfcf9aa60f9d59940f70f30ebfbbbf3207ba4
garyselios <garyseliosbennett@hotmail.com>
2026-03-18 20:26:48 -0300

    Implement MVC inventory by classification view

---
+++ controllers/invController.js
@@ -0,0 +1,21 @@
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
+
+const invController = {}
+
+/* 
+ *  Build inventory by classification view
+ */
+invController.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data)
+  let nav = await utilities.getNav()
+  res.render("./inventory/classification", {
+    title: "Classification",
+    nav,
+    grid,
+  })
+}
+
+module.exports = invController
