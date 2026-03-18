e4ffaaa856f5ef76d19ce78f044d64dc185fcad1
garyselios <garyseliosbennett@hotmail.com>
2026-03-18 20:55:52 -0300

    add error handling with 404 route and handleErrors middleware

--- routes/invRoutes.js
+++ routes/invRoutes.js
@@ -2,8 +2,9 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
 
-// Route to display vehicles by classification
-router.get("/type/:classificationId", invController.buildByClassificationId)
+// Route to display vehicles by classification (with error handling)
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
 
 module.exports = router
