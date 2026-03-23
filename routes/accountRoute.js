88600ef4bb2dbcc3fcf35f03018d0d4a6dd5693a
garyselios <garyseliosbennett@hotmail.com>
2026-03-23 20:07:49 -0300

    feat: add server-side validation for registration with express-validator

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation")  // ← NUEVO
 
 // Route to display login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -10,7 +11,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route to display registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-// Process registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process registration (with validation)
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router
